b9d77d22f3581e83d853ead38f4c92a66eba7ea3
HalilCan <halilcanmemoglu@gmail.com>
2020-01-05 10:49:53 +0300

    mostly done, doing PEP8 things

--- redditDL.py
+++ redditDL.py
@@ -1,32 +1,91 @@
 import urllib
 import os
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
 'note: the urllib save call is a separate instance'
 
-def clearPreviewUrl(url):
+
+def clear_preview_url(url):
     url = url.replace("preview", "i")
     url = url.split("?")[0]
     return url
 
-def saveMedia(mediaUrl, folderPath, name):
 
-    savePath = os.path.join(folderPath,)
+def save_media(media_url, folder_path, name):
+    save_path = os.path.join(folder_path, name)
+    urllib.urlretrieve(media_url, savePath)
+    return 0
 
-def downloadTopImagesInSubreddit(driver, address, maxCount, fullFolderPath):
 
 def unblock(driver):
     if "reddit.com: over 18?" in driver.title:
-        yesButton = driver.find_element_by_name("continue")
-        yesButton.click()
+        yes_button = driver.find_element_by_name("continue")
+        yes_button.click()
+        return 0
     else:
-        return
+        return 1
+
 
 def openPreviews(driver):
+    previewButtons = driver.find_elements_by_class_name("expando")
+    for button in previewButtons:
+        button.click()
+    return 0
 
 
-def convertAndSavePreviews(driver, previewImgArray):
+def getCorrectName(src, count):
+    extension = src.split(".")[-1]
+    name = os.path.join(str(count), extension)
+    return name
+
+
+def convert_and_save_from_previews(driver, count, folderPath):
+    previewImages = driver.find_elements_by_xpath("//div[@class='media-preview-content']/a")
+    downloadedCount = 0
+    for previewImage in previewImages:
+        src = previewImage.get_attribute("href")
+        if count > 0:
+            name = getCorrectName(src, count)
+            save_media(src, folderPath, name)
+            count -= 1
+            downloadedCount += 1
+        else:
+            break
+    if count > 0:
+        return -1
+    else:
+        return downloadedCount
+
+
+def downloadImageDirectlyFromExpandoList(driver):
+    previewButtons = driver.find_elements_by_class_name("expando")
+    for button in previewButtons:
+        'TODO if time improvement significant'
+
+
+def nextPage(driver):
+    try:
+        nextButton = driver.find_element_by_xpath("//span[@class='next-button']/a")
+        nextButton.click()
+    except NoSuchElementException:
+        return -1
+
+
+def downloadTopMediaInSubreddit(driver, address, count, fullFolderPath):
+    while count > 0:
+        unblockResult = unblock(driver)
+        openPreviewsResult = openPreviews(driver)
+        downloadedCount = convert_and_save_from_previews(driver, count, fullFolderPath)
+        if downloadedCount == -1:
+            return -1  # error while saving from previews
+        count = count - downloadedCount
+        nextPageResult = nextPage(driver)
+        if nextPageResult == -1:
+            return -2  # no more pages
+    return 0
+
 
 def main(argv):
     address = argv[1]
@@ -36,21 +95,11 @@ def main(argv):
     cwd = os.getcwd()
     fullFolderPath = os.path.join(cwd, folderPath)
 
-    downloadTopImagesInSubreddit(driver, address, maxCount, fullFolderPath)
-
-
-
     driver = webdriver.Firefox()
-    driver.get('http://www.google.com/recaptcha/demo/recaptcha')
+    driver.get(address)
+    downloadResult = downloadTopMediaInSubreddit(driver, address, maxCount, fullFolderPath)
 
-    # get the image source
-    img = driver.find_element_by_xpath('//div[@id="recaptcha_image"]/img')
-    src = img.get_attribute('src')
-
-    # download the image
-    urllib.urlretrieve(src, "captcha.png")
-
-    fullfilename = os.path.join(myPath, filename)
-    urllib.urlretrieve(url, fullfilename)
-
+    if downloadResult < 0:
+        driver.close()
+        return "HUMAN EXE ERROR"
     driver.close()
--- todo.md
+++ todo.md
@@ -1,5 +1,11 @@
 #TODO
-1. get basic structure in place
-2. implement structure
-3. handle waits
-4. handle existing downloads
+DONE get basic structure in place
+     implement structure
+     rudimentary end-to-end test
+     handle waits
+     handle existing downloads
+     handle different sorts from index
+     Turn downloader into an object to simplify and improve code
+     handle etc errors
+     handle different blocks
+     handle source indifference
